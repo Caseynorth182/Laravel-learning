7ea19ec7d1573e12fa9aacd288915d30bb5afdfb
Alexander Baranov <63122721+Caseynorth182@users.noreply.github.com>
2022-06-14 23:35:01 +0300

    'howe_work

---
+++ resources/views/laravel.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.app')
+
+
+
+@section('content')
+
+    <ul class="list-group p-2 m-4">
+        @foreach($data as $single_data)
+            <li class="list-group-item">{{$single_data}}</li>
+
+        @endforeach
+    </ul>
+@endsection
